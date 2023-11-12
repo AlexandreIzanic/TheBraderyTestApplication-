c0e8ce7a93380658ac4a136b18750bd590b04370
AlexandreIzanic <alexandre.izanic@gmail.com>
2023-11-13 00:48:33 +0100

    feat: search bar products

--- frontend/src/components/TableProducts.js
+++ frontend/src/components/TableProducts.js
@@ -1,23 +1,30 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import useProductStore from "../store/ProductsStore";
 import { CartContext } from "../context/CartContext";
 import { useContext } from "react";
 
 export default function TableProducts() {
   const productStore = useProductStore();
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     productStore.fetchProducts();
   }, []);
+
+  const filteredProducts = productStore.products
+    ? productStore.products.filter((product) =>
+        product.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    : [];
+
   console.log(productStore.products);
   const cart = useContext(CartContext);
 
   console.log(productStore.productData);
-
   console.log(cart.items);
 
-  const renderedData = productStore.products ? (
-    productStore.products.map((product) => {
+  const renderedData = filteredProducts.length ? (
+    filteredProducts.map((product) => {
       const productQuantity = cart.getProductQuantity(product.id);
 
       return (
@@ -82,21 +89,30 @@ export default function TableProducts() {
     })
   ) : (
     <tr>
-      <td colSpan="5">Loading products...</td>
+      <td colSpan="5">No matching products found.</td>
     </tr>
   );
 
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Price</th>
-          <th>inventory</th>
-        </tr>
-      </thead>
-      {renderedData}
-    </table>
+    <div>
+      <input
+        type="text"
+        placeholder="Search by name"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="mt-3 p-2"
+      />
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Inventory</th>
+          </tr>
+        </thead>
+        {renderedData}
+      </table>
+    </div>
   );
 }
